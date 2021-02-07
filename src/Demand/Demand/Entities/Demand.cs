e522d8c17b56e983247ff3b741320f53d6e1f5b1
Aleksandar Jakovljevic <aleksandar.jakovljevic@ezderm.com>
2021-02-07 20:24:50 +0100

    Demands API - Development - Part 2.

---
+++ src/Demand/Demand/Entities/Demand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Demands.API.Entites
+{
+    public class Demand
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public String Id { get; set; }
+
+        public Decimal Price { get; set; }
+
+        [BsonElement]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime ExpirationDate { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ src/Demand/Demand/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demands.API.Data;
+using Demands.API.Data.Interfaces;
+using Demands.API.Repositories;
+using Demands.API.Repositories.Interfaces;
+using Demands.API.Settings;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace Demands.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.Configure<DemandDatabaseSettings>(Configuration.GetSection(nameof(DemandDatabaseSettings)));
+
+            services.AddSingleton<IDemandDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DemandDatabaseSettings>>().Value);
+
+            services.AddTransient<IDemandContext, DemandContext>();
+            services.AddTransient<IDemandRepository, DemandRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
